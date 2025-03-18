06ac499e1ba7f9d3cbbed5c3d4b9e88b7069d95e
mey <meydetour@gmail.com>
2025-03-18 12:26:53 +0100

    fix : add date format

--- src/app/model/Booking.type.ts
+++ src/app/model/Booking.type.ts
@@ -4,6 +4,8 @@ export type Booking = {
   id: number,
   startDate: string,
   endDate: string,
+  startDateBaseFormat?: string,
+  endDateDateBaseFormat?: string,
   phoneNumber: string,
   mail: string,
   clientsNumber?: number,
