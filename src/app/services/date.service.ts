e588f2d6a29ede451e4d9330cef1ef9abc1e9687
mey <meydetour@gmail.com>
2025-03-18 12:24:40 +0100

    fix :  add verify date and change formate date

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
-import moment from 'moment/moment';
+import moment from 'moment';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,33 @@ export class DateService {
 
   formatDate(date: any,type:string) {
     if (date == "") return null
+      date = moment(date);
+    if (!date.isValid()) return null
+    if (type=="date"){
+     return  date.format("DD.MM.YYYY");
+    }
+    return date.format("DD.MM.YYYY HH:mm");
+  }
+  verifyDate(date: any,type:string) {
+    if (date == "" || !date){
+      console.log("date is empty or null")
+      return null
+    }
     if (type=="datetime"){
       date = moment(date);
     }else{
       date = moment(date, moment.ISO_8601, true);
     }
-    if (!date.isValid()) return null
-    return date.format("DD.MM.YYYY HH:mm");
+    if (!date.isValid()) {
+      console.log("date is invalid")
+      return null
+    }
+
+   //  if (type=="date"){
+   // return  date.format("DD/MM/YYYY");
+   //  }
+    return date.format("DD-MM-YYYYTHH:mm");
   }
+
+
 }
